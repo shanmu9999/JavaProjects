82564d2d0b382663b0ee15b1763bb4d09c9eb165
Badiga Shanmukha sai <shanmukha9.badiga@gmail.com>
2025-11-25 15:14:53 +0530

    Core Java

---
+++ src/com/barathtech/basicjava1/Helloworld2.java
@@ -0,0 +1,11 @@
+package com.barathtech.basicjava1;
+
+public class Helloworld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world2!");
+	}
+
+}
---
+++ src/com/barathtech/basicjava1/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package com.barathtech.basicjava1;
